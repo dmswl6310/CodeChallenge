987460ec4bdfad0e233d5e240b0a9dda2b2cd403
황은지 <dmswl6310@gmail.com>
2020-12-02 14:49:35 +0900

    완료

--- December/December/1012_유기농 배추.cpp
+++ December/December/1012_유기농 배추.cpp
@@ -1,43 +1,58 @@
 #include <iostream>
 #include <stack>
+#include <queue>
 using namespace std;
-int countW(stack <pair<int, int>> st, bool map[][]);
+int countW( bool map[][50],int M,int N,int K);
+void doBFS(bool map[][50], int M, int N, int strow, int stcol);
+int dirR[4] = { 0,1,0,-1 };
+int dirC[4] = { 1,0,-1,0 };
 
-int main() {
+void main2() {
 	int T, M, N, K;
 	cin >> T;
-	for (int i = 0; i < T) {
+	for (int i = 0; i < T;i++) {
 		int row, col;
 		cin >> M >> N >> K;
-		stack <pair<int, int>> st;
 		bool map[50][50] = { 0, };
-		for (int j = 0; j < k; j++) {
+		for (int j = 0; j < K; j++) {
 			cin >> col >> row;
 			map[row][col] = true;
-			st.push({ row,col });
 		}
-		cout << countW(st, map)
+		cout << countW(map,M,N,K)<<endl;
 	}
 
 
 }
 
-int countW(stack <pair<int, int>> st, bool map[][]) {
+int countW( bool map[][50], int M, int N, int K) {
 	int count = 0;
-	bool visit[50][50] = { 0, };
-	while (!st.empty()) {
-		int row = st.top().first;
-		int col = st.top().second;
-		st.pop();
-		if (visit[row][col] == false) {
-			count++;
-			visit[row][col] = true;
-			if (map[row + 1][col] == true && visit[row + 1][col] == false) st.push({ row + 1,col });
-			if (map[row - 1][col] == true && visit[row - 1][col] == false) st.push({ row - 1,col });
-			if (map[row][col + 1] == true && visit[row][col + 1] == false) st.push({ row,col + 1 });
-			if (map[row][col - 1] == true && visit[row][col - 1] == false) st.push({ row,col - 1 });
-
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (map[i][j]) {
+				count++;
+				doBFS(map, M, N,i,j);
+			}
 		}
 	}
 	return count;
+}
+
+void doBFS(bool map[][50], int M, int N,int strow,int stcol) {
+	queue <pair<int, int>> q;
+	map[strow][stcol] = false;
+	q.push({ strow, stcol });
+	while (!q.empty()) {
+		int row = q.front().first;
+		int col = q.front().second;
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+			int nextrow = row + dirR[i];
+			int nextcol = col + dirC[i];
+			if (nextrow>=0 && nextrow<N && nextcol>=0 && nextcol<M&&map[nextrow][nextcol]) {
+				map[nextrow][nextcol] = false;
+				q.push({ nextrow,nextcol });
+			}
+		}
+	}
+
 }
