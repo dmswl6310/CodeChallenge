22ddbcaa5f02e559ecfb0451f3b44071191e9586
황은지 <dmswl6310@gmail.com>
2020-12-07 18:52:56 +0900

    완료

--- December/December/sample.cpp
+++ December/December/sample.cpp
@@ -1,17 +1,16 @@
-#include<iostream>
-using namespace std;
-int main6() {
-	int max = 0;
-	int maxIndex = 0;
-	for (int i = 0; i < 9; i++) {
-		int num;
-		cin >> num;
-		if (num > max) {
-			max = num;
-			maxIndex = i + 1;
-		}
-}
-	cout << max << endl;
-	cout << maxIndex;
+#include <stdio.h>
+
+int main5() {
+	char names[][5] = { "aa","bb","cccc","dd" };
+
+	printf("%d\n", &names[3]);
+	printf("%d\n", &names[3][0]);
+
+	printf("%s\n", names[3]);
+//	printf("%s", names[3][0]); (°á°ú¾È¶ä)
+
+	printf("%c\n", names[3]);
+	printf("%c\n", names[3][0]);
+
 	return 0;
 }
